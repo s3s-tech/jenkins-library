e8533007e04f82e6ddf182715abbaaa7aa765343
anuppaudel1 <talkwithanup@gmail.com>
2017-10-28 02:41:00 -0400

    added library to call pet-clinic job

--- vars/petclinic.groovy
+++ vars/petclinic.groovy
@@ -1,11 +1,14 @@
 def call(body) {
     def config = [:]
+    //def slave = ${config.slave}
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
+    def slave = ${config.slave}
     body()
 
-node() {
-   git 'https://github.com/spring-projects/spring-petclinic.git'
+node(${slave}) {
+   //git 'https://github.com/spring-projects/spring-petclinic.git'
+   git '${config.git_url}'
    
    def server = Artifactory.server('${config.artifactory_server}')
    def rtMaven = Artifactory.newMavenBuild()
