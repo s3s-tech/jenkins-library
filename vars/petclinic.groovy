7b6492194195cf38ca4a7b9771cf7bc05216a046
anuppaudel1 <talkwithanup@gmail.com>
2017-10-28 17:29:11 -0400

    modified petclinic.groovy

--- vars/petclinic.groovy
+++ vars/petclinic.groovy
@@ -12,13 +12,13 @@ node {
    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/spring-projects/spring-petclinic.git']]])
 
    
-   def server = Artifactory.server('${config.artifactory_server}')
+   def server = Artifactory.server('${config.artifactoryServer}')
    def rtMaven = Artifactory.newMavenBuild()
    rtMaven.resolver server: server, releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot'
    rtMaven.deployer server: server, releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local'
    
    rtMaven.deployer.artifactDeploymentPatterns.addInclude("*.jar").addExclude("*.zip")
-rtMaven.tool = "${config.maven_name}"
+rtMaven.tool = "${config.mavenName}"
 // Run Maven:
 def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'clean install sonar:sonar'
 server.publishBuildInfo buildInfo
