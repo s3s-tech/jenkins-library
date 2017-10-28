c2d023dd7ae8dd678797e9c7aa1d45823b72b348
anuppaudel1 <talkwithanup@gmail.com>
2017-10-28 17:44:52 -0400

    modified petclinic.groovy

--- vars/petclinic.groovy
+++ vars/petclinic.groovy
@@ -12,7 +12,7 @@ node {
    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/spring-projects/spring-petclinic.git']]])
 
    
-   def server = Artifactory.server(''${config.artifactoryServer}'')
+   def server = Artifactory.server(${config.artifactoryServer})
    def rtMaven = Artifactory.newMavenBuild()
    rtMaven.resolver server: server, releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot'
    rtMaven.deployer server: server, releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local'
