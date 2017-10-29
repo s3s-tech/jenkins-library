b9e5fde81cba90e1af13ea63b2547d8452eea3e8
Bijaya Rijal <brijal6@gatech.edu>
2017-10-29 12:24:54 -0500

    Modified it for windows

--- vars/standardpipeline-anup.groovy
+++ vars/standardpipeline-anup.groovy
@@ -14,21 +14,21 @@ def call(body) {
                     checkout scm
                 }
                 stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
+                    bat "echo 'building ${config.projectName} ...'"
                 }
                 stage ('Tests') {
                     parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
+                        bat "echo 'shell scripts to run static tests...'"
                     },
                     'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
+                        bat "echo 'shell scripts to run unit tests...'"
                     },
                     'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
+                        bat "echo 'shell scripts to run integration tests...'"
                     }
                 }
                 stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                    bat "echo 'deploying to server ${config.serverDomain}...'"
                 }
             } catch (err) {
                 currentBuild.result = 'FAILED'
